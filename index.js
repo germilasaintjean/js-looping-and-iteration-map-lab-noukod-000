536c7016b4084e64900ad42bc3c2a02fc01aaf5a
Saint-Jean Germila <germila@noukod.com>
2019-03-06 18:16:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,12 @@
 function lowerCaseDrivers(drivers){
   return drivers.map(function(driverName){return driverName.toLowerCase()});
 }
+
+function nameToAttributes(drivers)
+ {
+  return drivers.map(function(driver){
+    const driverFirst = driver.split(' ')[0];
+    const driverLast = driver.split(' ')[1];
+    return {firstName: driverFirst, lastName: driverLast};
+    })
+};

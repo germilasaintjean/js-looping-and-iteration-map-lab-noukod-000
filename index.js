2af89430732f11971779765fcf464f0c41cd79f0
Saint-Jean Germila <germila@noukod.com>
2019-03-06 18:12:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Code your solution in this file.
 function lowerCaseDrivers(){
-  return drivers.map(function(driverName){return driverName.toLowerCase()
-  });
+  return drivers.map(function(driverName){return driverName.toLowerCase()});
 }
+
+
